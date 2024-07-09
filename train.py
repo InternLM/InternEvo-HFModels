dc5a427188b06e5e7152d9a30d6d7f78cd420444
zigzagcai <caizheng@pjlab.org.cn>
2024-07-09 15:51:27 +0800

    initial runnable huggingface_model

---
+++ train.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from internlm.core.context import global_context as gpc
+from internlm.core.trainer_builder import TrainerBuilder
+from internlm.data import (
+    build_train_loader_with_data_type,
+    build_valid_loader_with_data_type,
+)
+from internlm.initialize import initialize_distributed_env
+from internlm.monitor import internevo_monitor
+from internlm.train import initialize_model
+from internlm.utils.common import parse_args
+from internlm.model.registry import model_initializer, hf_config_initializer
+from huggingface_model.internlm_model.modeling_internlm import InternLMForCausalLM
+from huggingface_model.internlm_model.configuration_internlm import InternLMConfig
+
+
+@internevo_monitor(feishu_alert=True, clean_run=True)
+def main(args):
+
+    # register huggingface model and config for InternEvo
+    model_initializer.register_module("INTERNLM_FROM_HF", InternLMForCausalLM)
+    hf_config_initializer.register_module("INTERNLM_FROM_HF", InternLMConfig)
+
+    # initialize model
+    model = initialize_model()
+
+    # initialize train dataloader
+    train_dl, dataset_types = build_train_loader_with_data_type()
+
+    # initialize validation dataloader
+    val_dls = build_valid_loader_with_data_type()
+
+    # initialize kwargs
+    kwargs = vars(args) | {"dataset_types": dataset_types}
+
+    # build trainer
+    trainer = TrainerBuilder(model, train_dl, val_dls, **kwargs)
+
+    # training
+    trainer.fit()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # Initialize distributed environment
+    initialize_distributed_env(config=args.config, launcher=args.launcher, master_port=args.port, seed=args.seed)
+    assert hasattr(gpc, "config") and gpc.config is not None
+
+    # Run the main function with parsed arguments
+    main(args)
