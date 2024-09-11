b7e483b7b1b8e5e94236f9377b7fac295e3a1dfd
zigzagcai <caizheng@pjlab.org.cn>
2024-09-11 23:40:05 +0800

    add pack and isp training support for qwen2

--- examples/qwen2/train.py
+++ examples/qwen2/train.py
@@ -11,16 +11,17 @@ from internlm.initialize import initialize_distributed_env
 from internlm.monitor import internevo_monitor
 from internlm.utils.common import parse_args, get_current_device
 
-from huggingface_model.baichuan_inc.configuration_baichuan import BaichuanConfig
-from huggingface_model.baichuan_inc.modeling_baichuan import BaichuanForCausalLM
+from huggingface_model.qwen2.configuration_qwen2 import Qwen2Config
+from huggingface_model.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 
 
 @internevo_monitor(feishu_alert=True, clean_run=True)
 def main(args):
     # initialize model
-    model = BaichuanForCausalLM(
-        BaichuanConfig(
+    model = Qwen2ForCausalLM(
+        Qwen2Config(
             return_dict=False, 
+            _attn_implementation="flash_attention_2",
         )
     ).to(get_current_device())
 
---
+++ huggingface_model/qwen2/__init__.py
@@ -0,0 +1,4 @@
+from .modeling_qwen2 import Qwen2ForCausalLM
+from .configuration_qwen2 import Qwen2Config
+
+__all__ = ["Qwen2Config", "Qwen2ForCausalLM"]
